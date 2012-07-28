b2ec14b408c681286973384bb38785042b6fbf8b
Chris Kuehl <chris@techxonline.net>
2012-07-28 02:49:41 -0400

    Getting started with the Bootstrap framework.

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,10 @@
 modules = {
+    grailsDefault {
+        resource url: 'grails-default/js/application.js'
+    }
+    
     application {
-        resource url:'grails-default/js/application.js'
+        resource url: 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'
+        resource url: 'js/bootstrap.min.js'
     }
 }
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		<meta name="layout" content="grails-default"/>
+		<meta name="layout" content="main"/>
 		<title>Welcome to Grails</title>
 		<style type="text/css" media="screen">
 			#status {
