53339f58b01210a02e1e96559dc0cb0332577f26
Chris Kuehl <chris@techxonline.net>
2012-08-02 07:39:59 -0400

    Minor webService fixes.

--- grails-app/services/me/d19/nineteen/WebService.groovy
+++ grails-app/services/me/d19/nineteen/WebService.groovy
@@ -4,6 +4,6 @@ class WebService {
     def execService
     
     def updateDomain(def domain) { // usage: update.sh hostname username root useWWW useSSL javaPath
-        execService.exec("apache/update.sh ${domain.name} ${domain.account.name} ${domain.root} ${domain.www ? "www" : "no-www"} ${domain.ssl != null ? "ssl" : "no-ssl"} ${domain.javaContext == null ? "-" : domain.javaContext}")
+        execService.exec("apache/update.sh ${domain.name} ${domain.account.name} ${domain.root} ${domain.useWWW ? "www" : "no-www"} ${domain.sslCert != null ? "ssl" : "no-ssl"} ${domain.javaContext == null ? "-" : domain.javaContext}")
     }
 }
