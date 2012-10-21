b02d9e49419059ba8f98b4a18a202395de110a50
Chris Kuehl <chris@techxonline.net>
2012-10-20 22:57:48 -0400

    Fixing issue where database connection goes stale after not using for a while.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,7 +3,23 @@ dataSource {
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
+    
+    // resolve an issue with stale connections and Grails due to automatic
+    // connection pooling done by Grails (enabled above)
+    // http://stackoverflow.com/questions/2740987/mysql-connection-timeout-issue-grails-application-on-tomcat-using-hibernate-an
+    
+    // run the evictor every 30 minutes and evict any connections older than 30 minutes.
+    minEvictableIdleTimeMillis = 1800000
+    timeBetweenEvictionRunsMillis = 1800000
+    numTestsPerEvictionRun = 3
+    
+    // test the connection while its idle, before borrow and return it
+    testOnBorrow = true
+    testWhileIdle = true
+    testOnReturn = true
+    validationQuery = "SELECT 1"
 }
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
