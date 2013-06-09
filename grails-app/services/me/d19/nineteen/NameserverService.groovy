bf0de484ab496f0ea0d3caad89c0560a5d5813c6
Chris Kuehl <chris@techxonline.net>
2013-06-09 17:41:03 -0400

    Reorganize directory structure.

--- grails-app/services/me/d19/nineteen/NameserverService.groovy
+++
@@ -1,9 +0,0 @@
-package me.d19.nineteen
-
-class NameserverService {
-    def execService
-    
-    def updateDomain(Domain domain) {
-        execService.exec("bind/update.sh " + domain.name + " " + domain.serial)
-    }
-}
--- grails-app/taglib/me/d19/nineteen/UtilTagLib.groovy
+++
@@ -1,9 +0,0 @@
-package me.d19.nineteen
-
-class UtilTagLib {
-    def utilService
-    
-    def formatBytes = { attrs, body ->
-        out << utilService.formatBytes(attrs.bytes)
-    }
-}
