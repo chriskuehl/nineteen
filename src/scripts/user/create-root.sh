0e3556d74b0b358744547c8c07917e5a8b22755d
Chris Kuehl <chris@techxonline.net>
2013-10-25 10:00:37 -0700

    Rename logs -> log.

--- src/scripts/user/create-root.sh
+++ src/scripts/user/create-root.sh
@@ -2,11 +2,11 @@
 
 # create home roots
 HOME="/home/$1"
-mkdir -p "$HOME/logs/www/$2/"
+mkdir -p "$HOME/log/www/$2/"
 mkdir -p "$HOME/www/$2/public/"
 
 # set proper permissions
-chown -R $1:design19 "$HOME/logs/www/$2/"
+chown -R $1:design19 "$HOME/log/www/$2/"
 chown -R $1:design19 "$HOME/www/$2/"
 
 # all done
