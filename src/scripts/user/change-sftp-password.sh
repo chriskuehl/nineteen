e927f005dbac767e8e24ee110e05caf36e0fe893
Chris Kuehl <chris@techxonline.net>
2013-10-28 17:06:00 +0000

    Actually change SFTP password and propogate it to other servers.

--- src/scripts/user/change-sftp-password.sh
+++ src/scripts/user/change-sftp-password.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 USER="$1"
 PASSWORD="$2"
 
 HOME="/home/$USER"
 
 echo "$PASSWORD" > "$HOME/.nineteen/sftp-password"
+echo "$USER:$PASSWORD" | chpasswd
+
+# propogate the user to other servers
+/srv/scripts/update-users.sh
