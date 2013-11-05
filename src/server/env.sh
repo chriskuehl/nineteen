fda91009d4efc2e1a8810a45b10f49eb8956b620
Chris Kuehl <chris@techxonline.net>
2013-11-05 20:49:41 +0000

    Add protected users to environment variables.

--- src/server/env.sh
+++ src/server/env.sh
@@ -3,3 +3,6 @@
 # admin users
 # test:test
 export NINETEEN_ADMIN_TEST='$2a$10$W5oU6LKLmvY6EVcJrrpryuQyWVlxUmc/KZ4tGwc7zm9tNL81laPNS'
+
+# special users who can't be deleted (space-delimited)
+export NINETEEN_SPECIAL_USERS='nineteen chris'
