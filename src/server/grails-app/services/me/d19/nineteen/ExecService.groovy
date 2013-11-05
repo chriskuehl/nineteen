b84a639bfa2b042444474594dc46dc0b20c4c9ee
Chris Kuehl <chris@techxonline.net>
2013-11-05 07:02:19 +0000

    Remove old exec.

--- src/server/grails-app/services/me/d19/nineteen/ExecService.groovy
+++ src/server/grails-app/services/me/d19/nineteen/ExecService.groovy
@@ -19,29 +19,4 @@ class ExecService {
 
         return lines
 	}
-    
-    def exec(def cmd) {
-		def scriptPath = grailsApplication.config.nineteen.scriptPath
-        cmd = "sudo " + scriptPath + cmd + "\n"
-        
-        System.err.println("exec: " + cmd)
-        
-		def lines = []
-
-        try {
-            Process p = Runtime.getRuntime().exec(cmd)
-            p.waitFor()
-            
-            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()))
-            
-            while (r.ready()) {
-            	lines.add(r.readLine())
-            }
-        } catch (Throwable t) {
-            t.printStackTrace()
-            return null
-        }
-        
-        return lines
-    }
 }
