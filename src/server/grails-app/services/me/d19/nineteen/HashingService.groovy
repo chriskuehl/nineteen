879daa7922eb80cbc51f3e851280c4c16f381b7e
Chris Kuehl <chris@techxonline.net>
2013-11-05 07:17:14 +0000

    Add HashingService (from BrowseRight).

---
+++ src/server/grails-app/services/me/d19/nineteen/HashingService.groovy
@@ -0,0 +1,17 @@
+package me.d19.nineteen
+
+import org.mindrot.jbcrypt.BCrypt
+
+class HashingService {
+    def hash(def str) {
+        BCrypt.hashpw(str, BCrypt.gensalt(9))
+    }
+    
+    def hash(def str, def it) {
+        BCrypt.hashpw(str, BCrypt.gensalt(it.toInteger()))
+    }
+    
+    def matches(def hash, def str) {
+        BCrypt.checkpw(str, hash)
+    }
+}
